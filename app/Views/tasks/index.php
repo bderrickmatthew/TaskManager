2c186b5007f383bb4aead7ebca8d24c4c365d3c0
bderrickmatthew <bderrickmatthew@gmail.com>
2025-03-18 14:37:35 +0300

    formatted index.php

--- app/Views/tasks/index.php
+++ app/Views/tasks/index.php
@@ -9,60 +9,59 @@
 </form>
 
 <?php if (isset($_GET['error']) && !empty($_GET['error'])): ?>
-<div>
-    <p style="color:red;">
-        <strong>
-            <?= $_GET['error'] ?>
-        </strong>
-    </p>
-</div>
+    <div>
+        <p style="color:red;">
+            <strong>
+                <?= $_GET['error'] ?>
+            </strong>
+        </p>
+    </div>
 <?php endif; ?>
 
 <?php if (isset($_GET['success']) && !empty($_GET['success'])): ?>
-<div>
-    <p style="color:green;">
-        <strong>
-            <?= $_GET['success'] ?>
-        </strong>
-    </p>
-</div>
+    <div>
+        <p style="color:green;">
+            <strong>
+                <?= $_GET['success'] ?>
+            </strong>
+        </p>
+    </div>
 <?php endif; ?>
 
 <?php if (!empty($tasks)): ?>
-<h2>New Task Added:
-</h2>
-<?php foreach ($tasks as $index => $task): ?>
-<li style="margin-bottom: 5px;">
-    <?= $task->title; ?>
+    <h2>New Task Added:
+    </h2>
+    <?php foreach ($tasks as $index => $task): ?>
+        <li style="margin-bottom: 5px;">
+            <?= $task->title; ?>
 
-    <?php if (!$task->is_concluded): ?>
-    <form action="/tasks/completed" method="post" style="display: inline;"> <input type="hidden" name="csrf_token"
-            value="<?= \Bdm\TaskManager\System\CSRF::generateToken() ?>">
+            <?php if (!$task->is_concluded): ?>
+                    <form action="/tasks/completed" method="post" style="display: inline;"> <input type="hidden" name="csrf_token" value="<?= \Bdm\TaskManager\System\CSRF::generateToken() ?>">
 
-        <input type="hidden" name="id" value="<?= $task->id ?>">
-        <input type="hidden" name="action" value="markTaskAsCompleted">
-        <button type="submit" name="completed">Mark completed</button>
-    </form>
-    <?php else: ?>
+                    <input type="hidden" name="id" value="<?= $task->id ?>">
+                    <input type="hidden" name="action" value="markTaskAsCompleted">
+                    <button type="submit" name="completed">Mark completed</button>
+                </form>
+            <?php else: ?>
 
-    <strong style="color:green;">concluded</strong>
+                <strong style="color:green;">concluded</strong>
 
-    <?php endif; ?>
+            <?php endif; ?>
 
-    <form action="/tasks/delete" method="post" style="display:inline;">
+            <form action="/tasks/delete" method="post" style="display:inline;">
+                <input type="hidden" name="csrf_token" value="<?= \Bdm\TaskManager\System\CSRF::generateToken() ?>">
+
+                <input type="hidden" name="id" value="<?= $task->id ?>">
+                <input type="hidden" name="action" value="deleteTaskById">
+                <button type="submit" name="delete">delete</button>
+            </form>
+        </li>
+    <?php endforeach; ?>
+
+    <form action="/tasks/delete-all" method="post" style="margin-top: 25px;">
         <input type="hidden" name="csrf_token" value="<?= \Bdm\TaskManager\System\CSRF::generateToken() ?>">
 
-        <input type="hidden" name="id" value="<?= $task->id ?>">
-        <input type="hidden" name="action" value="deleteTaskById">
-        <button type="submit" name="delete">delete</button>
+
+        <button type="submit" name="clear">Clear All</button>
     </form>
-</li>
-<?php endforeach; ?>
-
-<form action="/tasks/delete-all" method="post" style="margin-top: 25px;">
-    <input type="hidden" name="csrf_token" value="<?= \Bdm\TaskManager\System\CSRF::generateToken() ?>">
-
-
-    <button type="submit" name="clear">Clear All</button>
-</form>
 <?php endif; ?>
